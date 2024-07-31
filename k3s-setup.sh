326e037dd2d66850b0040c2ed053b85d9ca8de1e
oshitikov <schitikov.ol@gmail.com>
2024-07-31 23:24:02 +0300

    remove unused qbittorrent chart, manage charts using helmfile

--- k3s-setup.sh
+++ k3s-setup.sh
@@ -1,11 +1,4 @@
-# REPLACE <changeme> OCCURRENCES IN .yaml CONFIGS!!!
 # RUN ON ANY HOST WITH KUBECTL ACCESS TO YOUR CLUSTER
 
-helm repo add jetstack https://charts.jetstack.io
-helm repo add longhorn https://charts.longhorn.io
-helm repo update
-
-helm upgrade --install cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace --set crds.enabled=true
-helm upgrade --install clusterissuer charts/clusterissuer --namespace cert-manager -f values/clusterissuer-values.yaml
-helm upgrade --install traefik-extra charts/traefik-extra --namespace traefik-extra --create-namespace -f values/traefik-extra-values.yaml
-helm upgrade --install longhorn longhorn/longhorn --namespace longhorn-system --create-namespace -f values/longhorn-values.yaml
+helmfile apply
+kubectl apply -f k3s/upgradeplan.yaml
