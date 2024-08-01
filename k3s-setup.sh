a5b8571b27014a3be8d11b490767fb63ac2aa945
oshitikov <schitikov.ol@gmail.com>
2024-08-01 15:01:07 +0300

    add needs to helmfile.yaml

--- k3s-setup.sh
+++ k3s-setup.sh
@@ -1,4 +1,6 @@
 # RUN ON ANY HOST WITH KUBECTL ACCESS TO YOUR CLUSTER
 
 helmfile apply
+kubectl apply -f https://github.com/rancher/system-upgrade-controller/releases/latest/download/system-upgrade-controller.yaml
+kubectl apply -f https://github.com/rancher/system-upgrade-controller/releases/latest/download/crd.yaml
 kubectl apply -f k3s/upgradeplan.yaml
