6c0042d5f1efb43fdaed47334fdca5f9973ae925
oshitikov <schitikov.ol@gmail.com>
2024-11-11 01:29:32 +0300

    add cert-manager default values, fix starr chart deployment

--- k3s-install.sh
+++ k3s-install.sh
@@ -4,5 +4,5 @@
 sudo mkdir -p /etc/rancher/k3s
 sudo cp k3s/config.yaml /etc/rancher/k3s/config.yaml
 curl -sfL https://get.k3s.io | sh -
-sudo cp k3s/local-storage-config.yaml /var/lib/rancher/k3s/server/manifests/local-storage-config.yam
+sudo cp k3s/local-storage-config.yaml /var/lib/rancher/k3s/server/manifests/local-storage-config.yaml
 sudo cp k3s/traefik-config.yaml /var/lib/rancher/k3s/server/manifests/traefik-config.yaml
